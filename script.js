8e0666043151a68f64ea9ef5bfadf92eeeab0d2a
mukhacheva <trrchacha666@gmail.com>
2024-11-30 02:02:37 +1000

    Added HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu').onclick = function () {
+    var topnav = document.getElementById('myTopnav');
+    topnav.classList.toggle('responsive'); // Переключаем класс, если его нет или он есть
+};
