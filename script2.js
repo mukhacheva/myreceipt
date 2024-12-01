5af5fbd5360c0a6e1ce98bfb74237f1def92c559
mukhacheva <trrchacha666@gmail.com>
2024-12-01 19:34:33 +1000

    ура

--- script2.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu').onclick = function () {
-    var topnav = document.getElementById('myTopnav');
-    topnav.classList.toggle('responsive'); // Переключаем класс, если его нет или он есть
-};
