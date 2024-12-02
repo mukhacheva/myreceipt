5f8384bcb643bbb694eba3070a4281d22a35e815
mukhacheva <trrchacha666@gmail.com>
2024-12-02 18:53:07 +1000

    knopka
    заработала кнопка

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.getElementById('menu').onclick = function () {
     var topnav = document.getElementById('myTopnav');
     topnav.classList.toggle('responsive'); // Переключаем класс, если его нет или он есть
 };
+
+function toggleMenu() {
+    const menu = document.getElementById("dropdownMenu");
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "flex";  // Показываем меню
+    } else {
+        menu.style.display = "none";  // Скрываем меню
+    }
+}
+
