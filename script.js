ec8d95f42a3b57140154468fefad91e71170ee96
mukhacheva <trrchacha666@gmail.com>
2024-12-04 14:04:02 +1000

    комментарии убрала

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 document.getElementById('menu').onclick = function () {
     var topnav = document.getElementById('myTopnav');
-    topnav.classList.toggle('responsive'); // Переключаем класс, если его нет или он есть
+    topnav.classList.toggle('responsive'); 
 };
 
-// Универсальная функция для переключения видимости меню
 function toggleMenu(menuId) {
     const menu = document.getElementById(menuId);
     
-    // Если меню скрыто или не видно, показываем его
     if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "flex";  // Показываем меню
+        menu.style.display = "flex";  
     } else {
-        menu.style.display = "none";  // Скрываем меню
+        menu.style.display = "none";  
     }
 }
 
-// Для закрытия меню при клике вне его
 document.addEventListener("click", function (event) {
     const menus = document.querySelectorAll('.dropdown-menu');
     menus.forEach(menu => {
         if (!menu.contains(event.target) && !menu.previousElementSibling.contains(event.target)) {
-            menu.style.display = "none"; // Скрываем меню
+            menu.style.display = "none"; 
         }
     });
 });
